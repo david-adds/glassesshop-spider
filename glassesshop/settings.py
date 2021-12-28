657793cfde3dd72c4620806b78631121837f51fd
david-adds <55636118+david-adds@users.noreply.github.com>
2021-12-28 15:23:43 -0300

    scraping data and Spoofing request headers

--- glassesshop/settings.py
+++ glassesshop/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = 'glassesshop.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'glassesshop (+http://www.yourdomain.com)'
+# USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -37,10 +37,9 @@ ROBOTSTXT_OBEY = True
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
+# DEFAULT_REQUEST_HEADERS = {
+#   'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
+# }
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
---
+++ glassesshop/spiders/glasses.py
@@ -0,0 +1,31 @@
+import scrapy
+
+
+class GlassesSpider(scrapy.Spider):
+    name = 'glasses'
+    allowed_domains = ['www.glassesshop.com']
+    
+    def start_requests(self):
+        yield scrapy.Request(url='https://www.glassesshop.com/bestsellers',callback=self.parse, headers={
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
+        })
+
+    def parse(self, response):
+        glasses =response.xpath("//div[@id='product-lists']/div")
+        for glasses_ in glasses:
+            yield{
+                'url': glasses_.xpath(".//div[@class='product-img-outer']/a/@href").get(),
+                'image_url': glasses_.xpath(".//img[@class='lazy d-block w-100 product-img-default']/@src").get(),
+                'product_name': glasses_.xpath("normalize-space(.//div[@class='p-title']/a/text())").get(),
+                'price': glasses_.xpath(".//div[@class='p-price']//span/text()").get(),
+                'User-Agent': response.request.headers['User-Agent']
+                
+                }
+            next_page = response.xpath(
+                "//lu[@class='pagination']/li[position()=last()]/a/@href").get()
+            if next_page:
+                yield scrapy.Request(url=next_page,callback=self.parse,headers={
+                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
+                    })
+
+            
